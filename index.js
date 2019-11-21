239d5136966eb6b9bf8bf4b4969507b6ff70a302
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-21 00:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+var dodger = document.getElementById('dodger');
 
-    dodger.style.left = `${left - 1}px`;
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
   }
-});
+}
 
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
   }
-});
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
