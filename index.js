2c188aa7cabfe39ebe0bd3528d4d83a1db351c08
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-21 00:03:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
+    dodger.style.left = `${left - 1}px`;
+  }
+});
