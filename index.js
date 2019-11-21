a486f3f80d8e585d0a168ae8e3f6e6f20487e16f
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-21 00:04:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
- 
+
     dodger.style.left = `${left - 1}px`;
   }
 });
